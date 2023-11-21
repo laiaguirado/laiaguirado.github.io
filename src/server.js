3a4d0368619d408001d9c15774bb43c73db397e6
Laia Guirado Campmany <laiaguirado1999@gmail.com>
2023-11-21 20:42:50 +0100

    fix local var

--- src/server.js
+++ src/server.js
@@ -10,15 +10,15 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use("/", express.static(config.FRONTEND_DIR));
+app.use("/", express.static("../../frontend / dist"));
 
 require('./trivial/trivial.controllers').addRoutesTo(app);
 
 const start = async () => {
     await db.connect();
-    app.listen(config.SERVER_PORT, () => {
-        const mode = config.NODE_ENV.toUpperCase();
-        console.log(`Trivial API Server (mode ${mode}) listening on port :${config.SERVER_PORT}`);
+    app.listen("8080", () => {
+        const mode = "development".toUpperCase();
+        console.log(`Trivial API Server (mode ${mode}) listening on port :8080`);
     });
 };
 
