ad34a7fe233811e45efab23c817f3f264a18967f
Laia Guirado Campmany <laiaguirado1999@gmail.com>
2023-11-23 20:57:33 +0100

    modify config connection

---
+++ config.js
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const envVarNames = [
+    "NODE_ENV",
+    "SERVER_PORT",
+    "DB_ATLAS_USER",
+    "DB_ATLAS_PASSWORD",
+    "DB_ATLAS_HOST",
+    "FRONTEND_DIR",
+    "DB_LOCAL_USER",
+    "DB_LOCAL_PASSWORD",
+    "DB_LOCAL_HOST",
+    "DB_LOCAL_PORT",
+    "DB_LOCAL_DATABASE",
+];
+
+let envVars = {};
+
+envVarNames.forEach(varName => {
+    if (process.env[varName] === undefined) {
+        throw new Error(`Missing environment variable '${varName}'`);
+    }
+    envVars[varName] = process.env[varName];
+})
+
+const getAtlasMongoURL = () => {
+    const { DB_ATLAS_USER, DB_ATLAS_PASSWORD, DB_ATLAS_HOST } = envVars;
+    return `mongodb+srv://${DB_ATLAS_USER}:${DB_ATLAS_PASSWORD}@${DB_ATLAS_HOST}/?retryWrites=true&w=majority`;
+}
+
+const getLocalMongoURL = () => {
+    const { DB_LOCAL_USER, DB_LOCAL_PASSWORD, DB_LOCAL_HOST, DB_LOCAL_PORT, DB_LOCAL_DATABASE } = envVars;
+    return `mongodb://${DB_LOCAL_USER}:${DB_LOCAL_PASSWORD}@${DB_LOCAL_HOST}:${DB_LOCAL_PORT}/${DB_LOCAL_DATABASE}?authSource=admin`;
+}
+
+export default {
+    ...envVars,
+    isDevelopment: process.env.NODE_ENV === "development",
+    LOCAL_MONGO_URL: getLocalMongoURL(),
+    ATLAS_MONGO_URL: getAtlasMongoURL()
+}
--- db.js
+++ db.js
@@ -1,18 +1,10 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import config from "./config.js"
 
-dotenv.config();
-const DB_ATLAS_PASSWORD = process.env.DB_ATLAS_PASSWORD;
-const DB_LOCAL_PASSWORD = process.env.DB_LOCAL_PASSWORD;
-const DB_USER = process.env.DB_USER;
-const DB_HOST = process.env.DB_HOST;
-const DB_PORT = process.env.DB_PORT;
-const DB_DATABASE = process.env.DB_DATABASE;
 
 const connect = async () => {
   try {
-    //let dataBaseUrl = `mongodb://${DB_USER}:${DB_LOCAL_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`;
-    let dataBaseUrl = `mongodb+srv://laiaguirado:${DB_ATLAS_PASSWORD}@cluster0.rtlkk76.mongodb.net/?retryWrites=true&w=majority`;
+    let dataBaseUrl = config.ATLAS_MONGO_URL;
     await mongoose.connect(dataBaseUrl);
     console.log("Mongoose connected");
   } catch (e) {
